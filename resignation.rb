9a14efd078bbc4ab9df2d187638fac4f77e6a249
Stephen Becker IV <sbeckeriv+git@gmail.com>
2012-04-12 09:29:54 -0700

    Update resignation.rb

--- resignation.rb
+++ resignation.rb
@@ -17,12 +17,12 @@ becker = Resignation.new("Becker","2012-04-27")
 becker.kind_note("I must cheer from the side lines now.")
 reasons = [
   "It is time for me to move on.",
-  "A T-rex told me in a dream to find a new job",
-  "I want to spend more time with my family",
-  "I'm joining the Peace Corps",
-  "My Facebook for Dogs startup got funded",
-  "I am going to buy storage lockers and get on TV",
-  "I need to devote more time to upvoting articles on Hacker News"
+  "A T-rex told me in a dream to find a new job.",
+  "I want to spend more time with my family.",
+  "I'm joining the Peace Corps.",
+  "My Facebook for Dogs startup got funded.",
+  "I am going to buy storage lockers and get on TV.",
+  "I need to devote more time to upvoting articles on Hacker News."
   ]
 becker.random_reasons(reasons)
 becker.resign!
