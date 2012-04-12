046ca2ec7b205e95aa7632817fa6c0748f361c40
Stephen Becker IV <sbeckeriv+git@gmail.com>
2012-04-12 10:45:24 -0700

    Update resignation.rb

--- resignation.rb
+++ resignation.rb
@@ -6,13 +6,14 @@ class Resignation
     @kind_note = "With great sorrow"
   end
   def random_reasons(reasons)
-    @reason_array=reasons
+    @reason_array=Array(reasons)
   end
   def resign!
     reason = reason_array ? reason_array[rand(reason_array.size)]+" " : ""
     puts "#{reason}#{kind_note} #{name} here by resigns effective as of #{date}.\n\n-#{name}"
   end
 end
+
 becker = Resignation.new("Becker","2012-04-27")
 becker.kind_note = "I must cheer from the side lines now."
 reasons = [
